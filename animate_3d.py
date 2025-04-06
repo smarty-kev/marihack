b6dd8153ef56258449e997a9d9d6653a6d3bf5bc
smarty-kev <86808153+smarty-kev@users.noreply.github.com>
2025-04-06 14:54:46 -0400

    Update animate_3d.py

--- animate_3d.py
+++ animate_3d.py
@@ -1,38 +1,50 @@
-import twod_ninety_degree_bounce_data
+import test
+
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+# --- Setup figure and 3D axis ---
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 100)
+ax.set_zlim(0, 100)
 
 xs = []
 ys = []
+zs = []
 
-
-for point in twod_ninety_degree_bounce_data.points:
+for point in test.points:
     xs.append(point[0])
     ys.append(point[1])
+    zs.append(point[2])
 
+# print(xs, ys, zs)
 
-fig, ax = plt.subplots()
-ax.set_xlim(0, 100)
-ax.set_ylim(0, 100)
-line, = ax.plot([], [], 'b-',animated=True, lw=2)  # blue line
-dot, = ax.plot([], [], 'ro', animated=True)         # red dot at the head
+# Create the dot
+dot, = ax.plot([], [], [], 'ro', markersize=6)
+line, = ax.plot([], [], [], 'b-', animated=True)
 
 
 def init():
     line.set_data([], [])
+    line.set_3d_properties([])
     dot.set_data([], [])
-    return line, dot
+    dot.set_3d_properties([])
+    return dot, line
 
-# Update function
+
 def update(frame):
-    line.set_data(xs[:frame], ys[:frame])  # draw path
-    dot.set_data([xs[frame]], [ys[frame]])  # current position
-    return line, dot
+    line.set_data(xs[:frame], ys[:frame])
+    line.set_3d_properties(zs[:frame])
+    dot.set_data([xs[frame]], [ys[frame]])  # Set x and y
+    dot.set_3d_properties([zs[frame]])
+    return dot, line
 
 
-ani = FuncAnimation(fig, update, frames=len(xs), init_func=init,
-                interval=1, blit=True)
+ani = FuncAnimation(fig, update, init_func=init,
+                    frames=len(xs), interval=20, blit=False)
 
-plt.gca().set_aspect('equal')
+plt.title("3D Bouncing Dot")
 plt.show()
+
